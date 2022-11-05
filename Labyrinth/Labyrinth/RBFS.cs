5ceb4509d753aef33c40c3b0610c2731aa0c36ac
aquaprogit <vladd.golovatyuk@gmail.com>
2022-11-05 03:37:51 +0200

    working rbfs

--- Labyrinth/Labyrinth/RBFS.cs
+++ Labyrinth/Labyrinth/RBFS.cs
@@ -5,12 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Labyrinth;
-internal class RBFS
+internal static class RBFS
 {
-    private SortedList<int, State> _prioritiets;
-
-    public RBFS()
+    private static OrderedList<State> _prioritiets = new OrderedList<State>(new StateComparer());
+    private static State _currentState = null!;
+    public static List<State> Solve(State state)
     {
-        _prioritiets = new SortedList<int, State>();
+        _currentState = state;
+        //int i = 0;
+        do
+        {
+            foreach (State child in _currentState.GetChildren())
+            {
+                _prioritiets.Add(child);
+            }
+            //  Console.WriteLine($"================={i}=====================");
+            _currentState = _prioritiets.First();
+            _prioritiets.RemoveAt(0);
+            //Console.WriteLine(_currentState);
+            //Console.WriteLine(_currentState.Maze);
+            //  Console.WriteLine($"================={i++}=====================");
+
+        } while (_currentState.Distance != 1 && _prioritiets.Count != 0);
+        List<State> result = _currentState.GetPath();
+        result.Reverse();
+        return result;
     }
 }
